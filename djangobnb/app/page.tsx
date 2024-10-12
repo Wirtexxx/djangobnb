6f3a296abc17c650c93450448a8c71825f7cd011
Vlad Haidaichuk <vlad.haidaichuk@gmail.com>
2024-10-12 21:07:31 +0200

    created categories list

--- djangobnb/app/page.tsx
+++ djangobnb/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Categories from "./components/Categories";
 
 export default function Home() {
-    return <main className="">Djangobnb</main>;
+    return (
+        <main className="max-w-[1500px] mx-auto px-6">
+            <Categories />
+        </main>
+    );
 }
